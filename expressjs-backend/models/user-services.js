c66190ba69dc5b8062216cae7f5ba84366b8660e
jubenjam <49247806+jubenjam@users.noreply.github.com>
2022-04-28 17:13:16 -0700

    CloudDatabaseImplemented

--- expressjs-backend/models/user-services.js
+++ expressjs-backend/models/user-services.js
@@ -1,12 +1,28 @@
 const mongoose = require("mongoose");
 const userModel = require("./user");
-mongoose.set("debug", true);
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// mongoose.set("debug", true);
 
 mongoose
-  .connect("mongodb://localhost:27017/users", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://" +
+      process.env.MONGO_USER +
+      ":" +
+      process.env.MONGO_PWD +
+      "@" +
+      process.env.MONGO_CLUSTER +
+      "/" +
+      process.env.MONGO_DB +
+      "?retryWrites=true&w=majority",
+    // "mongodb://localhost:27017/users",
+    {
+      useNewUrlParser: true, //useFindAndModify: false,
+      useUnifiedTopology: true,
+    }
+  )
   .catch((error) => console.log(error));
 
 async function getUsers(name, job) {
