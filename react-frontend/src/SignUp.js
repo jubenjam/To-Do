362bc68a5fef5bca9fedd4d6863fa82d66762d62
Arielle Goodman <goodmanr@calpoly.edu>
2022-05-16 13:37:21 -0700

    i dont know whats going on

---
+++ react-frontend/src/SignUp.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+function SignUp() {
+  const [email, setEmail] = useState();
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  const divStyle = {
+    position: 'absolute', left: '50%', top: '50%',
+    transform: 'translate(-50%, -50%)',
+    border: "5px solid black"
+  };
+
+  const inputStyle = {
+    margin: "20px",
+    width: "400px"
+  };
+
+  const buttonStyle = {
+    position: 'relative',
+    left: "300px",
+    bottom: "5px"
+  };
+
+
+
+  //style={{color: "red"}}
+
+  return(
+    <div className="SignUp" style={divStyle}>
+      <h1 style={inputStyle}>Sign Up</h1>
+      <form>
+      <label> 
+          <p style={inputStyle}>Email</p>
+          <input type="text" style={inputStyle} onChange={e => setEmail(e.target.value)}/>
+        </label>
+        <label> 
+          <p style={inputStyle}>Username</p>
+          <input type="text" style={inputStyle} onChange={e => setUserName(e.target.value)}/>
+        </label>
+        <label>
+          <p style={inputStyle}>Password</p>
+          <input type="password" style={inputStyle} onChange={e => setPassword(e.target.value)}/>
+        </label>
+        <div >
+          <button type="submit" style={buttonStyle}  >SignUp</button>
+        </div>
+        <p style={inputStyle}>Already have an account? <a href="http://localhost:3000/"> Login here</a></p>
+      </form>
+      
+    </div>
+  )
+}
+
+
+export default SignUp;
